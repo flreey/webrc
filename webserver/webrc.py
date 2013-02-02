4663d4473c05deb6413548b4c18f5a8997c615bf
flreey <flreey@gmail.com>
2013-02-03 01:21:29 +0800

    client can community with irc server

--- webserver/webrc.py
+++ webserver/webrc.py
@@ -1,6 +1,6 @@
 # -*- coding:utf-8 -*- 
 
-#import gevent
+import select, threading
 from tornado import web
 from tornadio2 import SocketConnection, TornadioRouter, SocketServer, event
 
@@ -22,22 +22,24 @@ class Socks(object):
         sock = cls.socks.pop(sockid, None)
         if sock:sock.close()
 
-    #@classmethod
-    #def select(cls):
-        #(rfds,wfds,efds) = gevent.select.select(cls.socks.values(), [], [])
-
-        #while True:
-            #if rfds:
-                #print rfds.recv(1024)
+def listen_irc_server(socks):
+    while True:
+        (rfds, wfds, efds) = select.select(socks.values(), [], [], 1)
+        for rfd in rfds:
+            data = rfd.recv(4086)
+            if data:
+                for k, v in socks.iteritems():
+                    if rfd == v:
+                        k.send(data)
 
 class IRCCommands(object):
     def send2irc(self, msg):
-        self.sock = Socks.create_socket(self.session.session_id)
+        self.sock = Socks.create_socket(self)
 
         try:
             self.sock.sendall(msg)
         except Exception as e:
-            Socks.close_socket(self.session.session_id)
+            Socks.close_socket(self)
             #close bidirect connection
             self.session.close()
             print e
@@ -46,7 +48,8 @@ class IRCCommands(object):
         self.send2irc('PRIVMSG #%s :%s\r\n' % (self.session.channel, msg))
 
     def recv_irc(self, bufsize=1024):
-        return self.sock.recv(bufsize)
+        recv = self.sock.recv(bufsize)
+        return recv
 
     def join(self, nick, password, channel):
         self.send2irc('CAP LS\r\nNICK '+nick+'\r\nUSER '+nick + ' ' + nick + ' 127.0.0.1 :'+nick+'\r\n');
@@ -69,15 +72,18 @@ class CharRoom(SocketConnection, IRCCommands):
         heartbeat = self.session._heartbeat
         def _heartbeat():
             heartbeat()
-            import time
-            self.send2irc('PING LAG%s12345\r\n' % str(int(time.time())))
+            try:
+                if self.session.channel:
+                    import time
+                    self.send2irc('PING LAG%s12345\r\n' % str(int(time.time())))
+            except Exception as e:
+                print e
+
         self.session._heartbeat = _heartbeat
-
-        print 'connection: %s' % self.session.session_id
-
+        print 'connection: %s' % self
     def on_close(self):
-        Socks.close_socket(self.session.session_id)
-        print 'disconnection: %s' % self.session.session_id
+        Socks.close_socket(self)
+        print 'disconnection: %s' % self
 
 # Create tornadio router
 ChatRouter = TornadioRouter(CharRoom)
@@ -89,11 +95,14 @@ application = web.Application(
     socket_io_port=8001
 )
 
-#gevent.spawn(Socks.select)
 
 if __name__ == "__main__":
     import logging
     logging.getLogger().setLevel(logging.DEBUG)
+
+    p = threading.Thread(target=listen_irc_server, args=(Socks.socks,))
+    p.setDaemon(True)
+    p.start()
 
     # Create and start tornadio server
     SocketServer(application)
